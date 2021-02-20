9f3020e1ee8dd6d43fa5c9a0fc583b76531933c3
Nikola Marunic <nikimarunic@gmail.com>
2021-02-20 11:56:21 -0500

    added react mic and playback plus encoding to base 64 string

--- my-app/src/Components/AudioView/App.js
+++ my-app/src/Components/AudioView/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { ReactMic } from "react-mic";
+import {
+    Container,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@material-ui/core";
 
 export class AudioView extends React.Component {
     constructor(props) {
@@ -10,6 +18,8 @@ export class AudioView extends React.Component {
         };
 
         this.onStop = this.onStop.bind(this);
+        this.onFileSelected = this.onFileSelected.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     startRecording = () => {
@@ -29,9 +39,42 @@ export class AudioView extends React.Component {
         this.setState({ blob: recordedBlob });
     }
 
+    onFileSelected(e) {
+        if (e.target.files.length > 0) {
+            let reader = new FileReader();
+
+            const callback = (url) => {
+                this.setState({
+                    scrnshot: url,
+                });
+            };
+            reader.onloadend = function () {
+                const dataUrl = reader.result;
+                // const image = new Image();
+                // image.src = dataUrl;
+                callback(dataUrl);
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    }
+
+    onSubmit(){
+
+       var reader = new FileReader();
+    //   reader.readAsDataURL(this.state.blob.blob); 
+       var superBuffer = new Blob([this.state.blob], {type: this.state.blob.options.mimeType});
+    //    reader.readAsDataURL(new Blob(this.state.blob, {type: 'video/webm'}));
+       reader.readAsDataURL(superBuffer);
+       reader.onloadend = function() {
+           var base64data = reader.result.split(",")[1];                
+           console.log(base64data);
+       }
+    }
+
     render() {
         return (
             <div>
+                <Container>
                 <ReactMic
                     record={this.state.record}
                     className="sound-wave"
@@ -46,7 +89,21 @@ export class AudioView extends React.Component {
                 <button onClick={this.stopRecording} type="button">
                     Stop
                 </button>
+                <Button onClick={() => {
+                                this.onSubmit();
+                            }}>Submit
+                </Button>
+                <Button variant="outlined" color="dark" component="label">
+                        Browse PC
+                        <input
+                            type="file"
+                            onChange={this.onFileSelected}
+                            accept="image/*"
+                            hidden
+                        />
+                </Button>
                 <audio controls src={this.state.blob.blobURL} />
+                </Container>
             </div>
         );
     }
