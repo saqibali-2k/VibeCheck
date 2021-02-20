827c4cab72ca8e7c2d0a5a40bcb24d3f2f7ad873
Faaris Ali <faarisali111@gmail.com>
2021-02-20 17:58:21 -0500

    added landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,54 @@ import "./App.css";
 import ImageView from "./Components/ImageView";
 import { AudioView } from "./Components/AudioView/App";
 import TextView from "./Components/TextView/App";
-import React from 'react';
-import Grid from '@material-ui/core'
+import { Grid, Button } from "@material-ui/core";
+import React from "react";
+
+const goBack = (setter, setShowLanding) => {
+    setter(false);
+    setShowLanding(true);
+};
+
+const goTo = (setter, setShowLanding) => {
+    setter(true);
+    setShowLanding(false);
+};
 
 function App() {
-    const showLanding, setShowLanding = react.useState(True)
-    const showImage, setShowImage = react.useState(false)
-    const showText, setShowText = react.useState(false)
+    const [showLanding, setShowLanding] = React.useState(true);
+    const [showImage, setShowImage] = React.useState(false);
+    const [showText, setShowText] = React.useState(false);
 
     return (
         <div className="App">
-            <ImageView />
+            {showText && (
+                <div>
+                    <Button onClick={() => goBack(setShowText, setShowLanding)}>
+                        Go Back
+                    </Button>
+                    <TextView />
+                </div>
+            )}
+            {showImage && (
+                <div>
+                    <Button
+                        onClick={() => goBack(setShowImage, setShowLanding)}
+                    >
+                        Go Back
+                    </Button>
+                    <ImageView />
+                </div>
+            )}
+            {showLanding && (
+                <div>
+                    <Button onClick={() => goTo(setShowText, setShowLanding)}>
+                        Image Analysis
+                    </Button>
+                    <Button onClick={() => goTo(setShowImage, setShowLanding)}>
+                        Text Analysis
+                    </Button>
+                </div>
+            )}
         </div>
     );
 }
