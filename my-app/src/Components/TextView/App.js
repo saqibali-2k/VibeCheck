839bad4725ae8b03d48e4ac8a953e2b400eaddd3
Faaris Ali <faarisali111@gmail.com>
2021-02-19 21:55:27 -0500

    centered textview

--- my-app/src/Components/TextView/App.js
+++ my-app/src/Components/TextView/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
 import React from "react";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Grid, Container, makeStyles } from "@material-ui/core";
 
-export default class TextView extends React.Component {
-    render() {
-        return (
-            <div>
+const styles = makeStyles({
+    button: {
+        marginTop: "17px",
+    },
+});
+
+export default function TextView() {
+    const classes = styles();
+
+    return (
+        <div>
+            <Container>
                 <Button>Go back</Button>
-                <h1>VIBE ✔</h1>
-                <TextField id="standard-basic" label="Enter your text here" />
-                <Button>Submit</Button>
-            </div>
-        );
-    }
+                <Grid container alignItems="flex-end" justify="center">
+                    <Grid>
+                        <h1>VIBE ✔</h1>
+                        <TextField
+                            id="standard-basic"
+                            label="Enter your text here"
+                        />
+                        <Button classes={{ root: classes.button }}>
+                            Submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
