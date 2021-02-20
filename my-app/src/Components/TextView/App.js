10a6358e36591a0bd4ce2ea5709f7c6584ce8dec
Nikola Marunic <nikimarunic@gmail.com>
2021-02-19 21:36:38 -0500

    added skeleton to text view

--- my-app/src/Components/TextView/App.js
+++ my-app/src/Components/TextView/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
 import React from "react";
-
-const styles = {
-    paperContainer: {
-        height: 1356,
-    },
-};
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class TextView extends React.Component {
     render() {
         return (
             <div>
-                <p>test</p>
+                <Button>Go back</Button>
+                <h1>VIBE ✔</h1>
+                <TextField id="standard-basic" label="Enter your text here" />
+                <Button>Submit</Button>
             </div>
         );
     }
